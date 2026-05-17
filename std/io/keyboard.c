7f20d863a9b8289454ef2b30fb6ec57cf787797c
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-05-17 10:37:03 +0900

    いろいろ書式を考える

--- std/io/keyboard.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "keyboard.h"
-
-int kome_std_io_keyboard_onPress(char* key) {
-    return scanf("%s", key);
-}
--- std/io/keyboard.h
+++ std/io/keyboard.h
@@ -1,6 +1,6 @@
 #ifndef KOME_STD_IO_KEYBOARD_H
 #define KOME_STD_IO_KEYBOARD_H
 
-int kome_std_io_keyboard_onPress(char* key);
+
 
 #endif /* KOME_STD_IO_KEYBOARD_H */
