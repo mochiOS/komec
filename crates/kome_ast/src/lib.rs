e2ec32d70fbeadb00cd8978fef05327c51261e72
098orin <098orin0102@gmail.com>
2026-06-14 16:41:36 +0900

    Create lib.rs

---
+++ crates/kome_ast/src/lib.rs
@@ -0,0 +1,22 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Span {
+    pub start: usize,
+    pub end: usize,
+}
+
+impl Span {
+    pub const fn new(start: usize, end: usize) -> Self {
+        Self { start, end }
+    }
+
+    pub fn merge(self, other: Self) -> Self {
+        Self {
+            start: self.start.min(other.start),
+            end: self.end.max(other.end),
+        }
+    }
+}
+
+pub trait AstNode {
+    fn span(&self) -> Span;
+}
