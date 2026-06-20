58c68fc2be31d2886282ed6a02a2c72695d7b007
098orin <098orin0102@gmail.com>
2026-06-20 17:25:10 +0900

    Add alloc helpers and scope stack management

--- crates/kome_semantics/src/resolver.rs
+++ crates/kome_semantics/src/resolver.rs
@@ -23,4 +23,41 @@ impl ScopeBuilder {
             next_symbol_id: 0,
         }
     }
+
+    fn alloc_scope(&mut self) -> ScopeId {
+        let id = self.next_scope_id;
+        self.next_scope_id += 1;
+        id
+    }
+
+    fn alloc_symbol(&mut self) -> SymbolId {
+        let id = self.next_symbol_id;
+        self.next_symbol_id += 1;
+        id
+    }
+
+    fn current_scope(&self) -> ScopeId {
+        *self.scope_stack.last().unwrap()
+    }
+
+    fn enter_scope(&mut self, kind: ScopeKind) -> ScopeId {
+        let parent = self.scope_stack.last().copied();
+        let id = self.alloc_scope();
+        self.scopes.push(Scope {
+            id,
+            parent,
+            kind,
+            symbols: Vec::new(),
+            children: Vec::new(),
+        });
+        if let Some(parent_id) = parent {
+            self.scopes[parent_id].children.push(id);
+        }
+        self.scope_stack.push(id);
+        id
+    }
+
+    fn exit_scope(&mut self) {
+        self.scope_stack.pop();
+    }
 }
