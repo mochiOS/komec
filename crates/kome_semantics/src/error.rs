0162d8d59bfd2a2a06f4c6ef6b3cb91e1ae36afa
098orin <098orin0102@gmail.com>
2026-06-20 17:23:51 +0900

    Add ResolutionError enum

---
+++ crates/kome_semantics/src/error.rs
@@ -0,0 +1,12 @@
+use kome_ast::Span;
+
+#[derive(Debug, Clone)]
+pub enum ResolutionError {
+    UndefinedName { name: String, span: Span },
+    DuplicateDefinition {
+        name: String,
+        first: Span,
+        second: Span,
+    },
+    ScopeStackEmpty,
+}
